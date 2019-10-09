4a6db14981ead669fbac6708a9bdc50f564b7951
Wolfpacker <52385943+iestarks@users.noreply.github.com>
2019-10-09 15:33:35 -0400

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+    subscription_id = "125d63cb-81b9-4e7e-acf5-befe71e0f04b"
+    client_id       = "b4c82e3a-4712-4cfb-9549-c8fe1dd22188"
+    client_secret   = ""
+    tenant_id       = "7f74305b-aab9-4436-9e02-ff1ec6c8e1ab"
+}
